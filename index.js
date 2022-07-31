acb8b11bf1020eb13c1fe2ba1c967d71c86325b3
yogendrasinghrawat <git.yrawat@gmail.com>
2022-07-31 17:00:25 +0530

    fix a type error in dishes.js

--- index.js
+++ index.js
@@ -18,11 +18,23 @@ connect.then((db) => {
     .then((dish) => {
       console.log(dish);
 
-      return Dishes.find({});
+      return Dishes.findByIdAndUpdate(
+        dish._id,
+        { $set: { description: "Updated test" } },
+        { new: true }
+      ).exec();
     })
-    .then((dishes) => {
-      console.log(dishes);
-
+    .then((dish) => {
+      console.log(dish);
+      dish.comments.push({
+        rating: 5,
+        comment: "it's good!",
+        author: "leo",
+      });
+      return dish.save();
+    })
+    .then((dish) => {
+      console.log(dish);
       return Dishes.deleteOne({});
     })
     .then(() => {
